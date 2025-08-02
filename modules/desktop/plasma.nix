20f0274074c439ea947f48ac8584b077d984169d
Keith Calvelli <keith@calvelli.dev>
2025-08-02 14:42:53 -0400

    I am not a smart man

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -28,8 +28,10 @@
     };
 
     services.desktopManager.plasma6.enable = true;
-    services.displayManager.sddm.enable = true;
-    services.displaymanager.sddm.wayland.enable = true;
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
     services.flatpak.enable = true;    
 
     environment.systemPackages = with pkgs; [
