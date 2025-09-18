64e33ac1345f40e29eebd7b79abd8a4330d5286a
Keith Calvelli <keith@calvelli.dev>
2025-09-18 15:37:27 -0400

    Spec-kit devshell

--- devshells/spec.nix
+++ devshells/spec.nix
@@ -1,6 +1,6 @@
 {
   pkgs,
-  inputs,   # <- this is inputs'
+  inputs,   # this is inputs'
   system
 }:
 let
@@ -8,11 +8,30 @@ let
 in
 mkShell {
   name = "spec-kit";
-  packages = [ pkgs.python311 pkgs.uv pkgs.git pkgs.gh pkgs.nodejs_20 ];
-  commands = [
-    { name = "specify";    help = "Run GitHub Spec Kit CLI via uvx";
-      command = ''uvx --from git+https://github.com/github/spec-kit.git specify "$@"''; }
-    { name = "spec-check"; help = "Check Spec Kit prerequisites"; command = "specify check"; }
+
+  packages = [
+    pkgs.python311
+    pkgs.uv
+    pkgs.git
+    pkgs.gh
+    pkgs.nodejs_20
   ];
-  env = { PIP_DISABLE_PIP_VERSION_CHECK = "1"; UV_NO_SYNC = "1"; };
+
+  commands = [
+    {
+      name = "specify";
+      help = "Run GitHub Spec Kit CLI via uvx";
+      command = ''uvx --from git+https://github.com/github/spec-kit.git specify "$@"'';
+    }
+    {
+      name = "spec-check";
+      help = "Check Spec Kit prerequisites";
+      command = "specify check";
+    }
+  ];
+
+  env = [
+    { name = "PIP_DISABLE_PIP_VERSION_CHECK"; value = "1"; }
+    { name = "UV_NO_SYNC"; value = "1"; }
+  ];
 }
